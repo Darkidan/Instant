ac6aec43e8a1064c16ed0591d02a87b96c0dbef2
Idan Bing <darkidan@gmail.com>
2018-11-13 14:05:56 +0200

    Initial Commit

---
+++ Proj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proj
+//
+//  Created by Darkidan on 13/11/2018.
+//  Copyright © 2018 Darkidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
